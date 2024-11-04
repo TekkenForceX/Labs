24570edcb32a73bdf5142eafab2ac6ff4d320ea8
Matthew Fails <matthew.c.fails.com>
2024-11-04 18:32:34 -0500

    All three labs

---
+++ lab-4/script.js
@@ -0,0 +1,19 @@
+let firstName = "Matthew";
+let lastName = "Fails";
+const fullName = firstName + " " + lastName;
+
+let detroitPopulation =  631524;
+let javascriptIsCool = Boolean(true);
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+console.log(fullName)
+console.log(detroitPopulation)
+console.log(detroitPopulation * 3)
+
+for (let num = 1; num < 6; num++) {
+    console.log(num)
+}
+
+for (let num = 0; num < 5; num++) {
+    console.log(num)
+}
